c0443d115b6c4b704df18aa1ab08131f8214f24a
Diggerati <innocentmasuki99@gmail.com>
2020-05-03 04:32:28 +0300

    putting card matches

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 var cardsContainer = document.getElementById("game-container");
 var newGame_btn = document.getElementById('new-game-btn');
 var icons = document.getElementsByClassName("fa");
+var selectedId, count = 0;
 
-
-function addCards(container, cards){
-    cards.forEach(function (child){
+function addCards(container, cards) {
+    cards.forEach(function (child) {
         container.appendChild(child);
     });
 }
 
-function generateCards(idValue, iconClass){
+function generateCards(idValue, iconClass) {
     card = document.createElement('button');
     card.setAttribute('class', 'game-card');
     card.setAttribute('id', idValue);
@@ -57,15 +57,16 @@ var cardsView = [
 
 
 function randomArrayShuffle(card) {
-  var currentIndex = card.length, temporaryValue, randomIndex;
-  while (0 !== currentIndex) {
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex -= 1;
-    temporaryValue = card[currentIndex];
-    card[currentIndex] = card[randomIndex];
-    card[randomIndex] = temporaryValue;
-  }
-  return card;
+    var currentIndex = card.length,
+        temporaryValue, randomIndex;
+    while (0 !== currentIndex) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        temporaryValue = card[currentIndex];
+        card[currentIndex] = card[randomIndex];
+        card[randomIndex] = temporaryValue;
+    }
+    return card;
 }
 
 
@@ -76,7 +77,7 @@ function randomArrayShuffle(card) {
 
 
 
-function startGame(){
+function startGame() {
     var new_game_cards = randomArrayShuffle(cardsView);
     addCards(cardsContainer, new_game_cards);
 
@@ -84,31 +85,54 @@ function startGame(){
 
 
 
-function newGame(){
+function newGame() {
     startGame();
 }
 
 
 
-function show(id){
+
+function show(id) {
     var selectedCard = document.getElementById(id);
     ico = selectedCard.children[0];
-    if(ico.style.display === "block"){
-            ico.style.display = "none";
-    }
-    else{
-            ico.style.display = "block";
-    }
-        selectedCard.disabled = "true";
+    ico.classList.add("open");
+    selectedId = id;
+    count++;
+    selectedCard.disabled = "true";
 
-    return id;
 }
 
+function match() {
+    var choice1, choice2;
+    if (count == 1) {
+        choice1 = selectedId;
+        console.log(choice1);
+        console.log(count);
+    } else if (count == 2) {
+        choice2 = selectedId;
+        console.log(choice2);
+        console.log(count);
+    }
+
+    if ((choice1 === "1") && (choice2 === "9")) {
+        console.log("Matched");
+    } else if ((choice1 === "2") && (choice2 === "10")) {
+        console.log("Matched");
+    } else if ((choice1 === "3") && (choice2 === "11")) {
+        console.log("Matched");
+    } else if ((choice1 === "4") && (choice2 === "12")) {
+        console.log("Matched");
+    } else if ((choice1 === "5") && (choice2 === "13")) {
+        console.log("Matched");
+    } else if ((choice1 === "6") && (choice2 === "14")) {
+        console.log("Matched");
+    } else if ((choice1 === "7") && (choice2 === "15")) {
+        console.log("Matched");
+    } else if ((choice1 === "8") && (choice2 === "16")) {
+        console.log("Matched");
+    } else {
+        console.log("Stupid");
+    }
 
 
-
-
-
-
-
-
+}
