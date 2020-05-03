b332ee2b149057b63d58835ee542073eca31ec9a
Diggerati <innocentmasuki99@gmail.com>
2020-05-04 02:33:33 +0300

    started again

--- main.js
+++ main.js
@@ -1,9 +1,7 @@
-var cardsContainer = document.getElementById("game-container");
-var newGame_btn = document.getElementById('new-game-btn');
-var icons = document.getElementsByClassName("fa");
-var selectedId, count = 0,
-    choice1, choice2;
-
+let cardsContainer = document.getElementById("game-container");
+// let newGame_btn = document.getElementById('new-game-btn');
+let icons = document.getElementsByClassName("fa");
+let selectedId, history = [];
 
 
 function addCards(container, cards) {
@@ -13,7 +11,7 @@ function addCards(container, cards) {
 }
 
 function generateCards(idValue, iconClass) {
-    card = document.createElement('button');
+    let card = document.createElement('button');
     card.setAttribute('class', 'game-card');
     card.setAttribute('id', idValue);
     card.setAttribute('onmouseup', 'show(this.id)');
@@ -21,40 +19,39 @@ function generateCards(idValue, iconClass) {
     return card;
 }
 
-
-var icontype = {
+ const iconType = {
     instagram: "fa fa-instagram",
-    whatsapp: "fa fa-whatsapp",
+    whatsApp: "fa fa-whatsapp",
     snapchat: "fa fa-snapchat-ghost",
     facebook: "fa fa-facebook-official",
     twitter: "fa fa-twitter",
-    wechat: "fa fa-wechat",
+    weChat: "fa fa-wechat",
     googlePlus: "fa fa-google-plus",
     telegram: "fa fa-telegram"
 };
 
-var cardsView = [
-    generateCards(1, icontype.instagram),
-    generateCards(2, icontype.whatsapp),
-    generateCards(3, icontype.snapchat),
-    generateCards(4, icontype.facebook),
-    generateCards(5, icontype.twitter),
-    generateCards(6, icontype.wechat),
-    generateCards(7, icontype.googlePlus),
-    generateCards(8, icontype.telegram),
-    generateCards(9, icontype.instagram),
-    generateCards(10, icontype.whatsapp),
-    generateCards(11, icontype.snapchat),
-    generateCards(12, icontype.facebook),
-    generateCards(13, icontype.twitter),
-    generateCards(14, icontype.wechat),
-    generateCards(15, icontype.googlePlus),
-    generateCards(16, icontype.telegram)
+const cardsView = [
+    generateCards(1, iconType.instagram),
+    generateCards(2, iconType.whatsApp),
+    generateCards(3, iconType.snapchat),
+    generateCards(4, iconType.facebook),
+    generateCards(5, iconType.twitter),
+    generateCards(6, iconType.weChat),
+    generateCards(7, iconType.googlePlus),
+    generateCards(8, iconType.telegram),
+    generateCards(9, iconType.instagram),
+    generateCards(10, iconType.whatsApp),
+    generateCards(11, iconType.snapchat),
+    generateCards(12, iconType.facebook),
+    generateCards(13, iconType.twitter),
+    generateCards(14, iconType.weChat),
+    generateCards(15, iconType.googlePlus),
+    generateCards(16, iconType.telegram)
 ];
 
 
 function randomArrayShuffle(card) {
-    var currentIndex = card.length,
+    let currentIndex = card.length,
         temporaryValue, randomIndex;
     while (0 !== currentIndex) {
         randomIndex = Math.floor(Math.random() * currentIndex);
@@ -68,75 +65,55 @@ function randomArrayShuffle(card) {
 
 
 function startGame() {
-    var new_game_cards = randomArrayShuffle(cardsView);
+    let new_game_cards = randomArrayShuffle(cardsView);
     addCards(cardsContainer, new_game_cards);
 
 }
 
 
 function hideIcons(){
-    for(var i = 0; i < icons.length; i++){
+    for(let i = 0; i < icons.length; i++){
         icons[i].style.display = "none";
+    }
+}
+
+function viewIcons(){
+    for(let i = 0; i < icons.length; i++){
+        icons[i].style.display = "block";
     }
 }
 
 
 function newGame() {
     startGame();
+    viewIcons();
     window.setTimeout(hideIcons, 5000);
 
 }
 
 
-
-
-function show(id) {
-     var selectedCard = document.getElementById(id);
-    var ico = selectedCard.children[0];
-    ico.style.display = "block";
-    selectedId = id;
-    count++;
-    selectedCard.disabled = "true";
-    match();
-
+function idPusher(id) {
+    history.push(id);
+    console.log(history);
 }
 
+function conditions(){
+    if ((history[0] === "1") && (history[1] === "9") || (history[0] === "9") && (history[1] === "1")) {
+        return "yes";
+    } else if ((history[0] === "2") && (history[1] === "10") || (history[0] === "10") && (history[1] === "2")) {
+        return "yes";
+    } else if ((history[0] === "3") && (history[1] === "11") || (history[0] === "11") && (history[1] === "3")) {
+        return "yes";
+    } else if ((history[0] === "4") && (history[1] === "12") || (history[0] === "12") && (history[1] === "4")) {
+        return "yes";
+    } else if ((history[0] === "5") && (history[1] === "13") || (history[0] === "13") && (history[1] === "5")) {
+        return "yes";
+    } else if ((history[0] === "6") && (history[1] === "14") || (history[0] === "14") && (history[1] === "6")) {
+        return "yes";
+    } else if ((history[0] === "7") && (history[1] === "15") || (history[0] === "15") && (history[1] === "7")) {
+        return "yes";
 
-function match() {
-    switch (count) {
-        case 1:
-            choice1 = selectedId;
-            console.log(count);
-            console.log(choice1);
-            break;
-        case 2:
-            choice2 = selectedId;
-            console.log(count);
-            console.log(choice2);
-            count = 0;
-            break;
-        default:
-            break;
-    }
-
-
-  function conditions(){
-    if ((choice1 == "1") && (choice2 == "9") || (choice1 == "9") && (choice2 == "1")) {
-        return "yes";
-    } else if ((choice1 == "2") && (choice2 == "10") || (choice1 == "10") && (choice2 == "2")) {
-        return "yes";
-    } else if ((choice1 == "3") && (choice2 == "11") || (choice1 == "11") && (choice2 == "3")) {
-        return "yes";
-    } else if ((choice1 == "4") && (choice2 == "12") || (choice1 == "12") && (choice2 == "4")) {
-        return "yes";
-    } else if ((choice1 == "5") && (choice2 == "13") || (choice1 == "13") && (choice2 == "5")) {
-        return "yes";
-    } else if ((choice1 == "6") && (choice2 == "14") || (choice1 == "14") && (choice2 == "6")) {
-        return "yes";
-    } else if ((choice1 == "7") && (choice2 == "15") || (choice1 == "15") && (choice2 == "7")) {
-          return "yes";
-
-    } else if ((choice1 == "8") && (choice2 == "16") || (choice1 == "16") && (choice2 == "8")) {
+    } else if ((history[0] === "8") && (history[1] === "16") || (history[0] === "16") && (history[1] === "8")) {
         return "yes";
     } else {
         return "no";
@@ -144,12 +121,41 @@ function match() {
     }
 }
 
-    if((count == 0) && (conditions() == "yes")){
+function match() {
+
+    if((history.length === 2) && (conditions() === "yes")){
         console.log("good");
-    }else {
-        console.log("choose another");
+    }else if ((history.length === 2) && (conditions() === "no")) {
+
+    }
+
+    if (history.length === 3){
+        history,length = 0;
     }
 
 
 }
+
+
+
+function show(id) {
+    let selectedCard = document.getElementById(id);
+    let ico = selectedCard.children[0];
+    ico.style.display = "block";
+    selectedId = id;
+    selectedCard.disabled = "true";
+    idPusher(selectedId);
+    match();
+
+}
+
+// cardsContainer.addEventListener('click', function () {
+//     let selectedCard = document.getElementById(this.id);
+//     let ico = selectedCard.children[0];
+//     ico.style.display = "block";
+//     selectedId = this.id;
+//     count++;
+//     selectedCard.disabled = "true";
+//     match();
+// });
 
