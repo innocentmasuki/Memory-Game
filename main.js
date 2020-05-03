a45d2f27c47f71eb908a09e320bcf40ab88faf15
Diggerati <innocentmasuki99@gmail.com>
2020-05-03 05:28:10 +0300

    adding some functionality

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 var cardsContainer = document.getElementById("game-container");
 var newGame_btn = document.getElementById('new-game-btn');
 var icons = document.getElementsByClassName("fa");
-var selectedId, count = 0;
+var selectedId, count = 0, choice1, choice2;
+
+
 
 function addCards(container, cards) {
     cards.forEach(function (child) {
@@ -99,40 +101,52 @@ function show(id) {
     selectedId = id;
     count++;
     selectedCard.disabled = "true";
+    match();
 
 }
 
+
 function match() {
-    var choice1, choice2;
-    if (count == 1) {
-        choice1 = selectedId;
-        console.log(choice1);
+    switch(count){
+        case 1:
+           choice1 = selectedId;
         console.log(count);
-    } else if (count == 2) {
-        choice2 = selectedId;
+            console.log(choice1);
+            break;
+        case 2:
+            choice2 = selectedId;
+        console.log(count);
         console.log(choice2);
-        console.log(count);
+        console.log(choice1);
+            count = 0;
+            break;
+        default:
+            break;
     }
 
-    if ((choice1 === "1") && (choice2 === "9")) {
-        console.log("Matched");
-    } else if ((choice1 === "2") && (choice2 === "10")) {
-        console.log("Matched");
-    } else if ((choice1 === "3") && (choice2 === "11")) {
-        console.log("Matched");
-    } else if ((choice1 === "4") && (choice2 === "12")) {
-        console.log("Matched");
-    } else if ((choice1 === "5") && (choice2 === "13")) {
-        console.log("Matched");
-    } else if ((choice1 === "6") && (choice2 === "14")) {
-        console.log("Matched");
-    } else if ((choice1 === "7") && (choice2 === "15")) {
-        console.log("Matched");
-    } else if ((choice1 === "8") && (choice2 === "16")) {
-        console.log("Matched");
+
+
+    if ((choice1 == "1") && (choice2 == "9") || (choice1 == "9") && (choice2 == "1")) {
+        console.log("Matched Instagram");
+    } else if ((choice1 == "2") && (choice2 == "10") || (choice1 == "10") && (choice2 == "2")) {
+        console.log("Matched WhatsApp");
+    } else if ((choice1 == "3") && (choice2 == "11") || (choice1 == "11") && (choice2 == "3")) {
+        console.log("Matched snapchat");
+    } else if ((choice1 == "4") && (choice2 == "12") || (choice1 == "12") && (choice2 == "4")) {
+        console.log("Matched facebook");
+    } else if ((choice1 == "5") && (choice2 == "13") || (choice1 == "13") && (choice2 == "5")) {
+        console.log("Matched twitter");
+    } else if ((choice1 == "6") && (choice2 == "14") || (choice1 == "14") && (choice2 == "6")) {
+        console.log("Matched wechat");
+    } else if ((choice1 == "7") && (choice2 == "15") || (choice1 == "15") && (choice2 == "7")) {
+        console.log("Matched gplus");
+    } else if ((choice1 == "8") && (choice2 == "16") || (choice1 == "16") && (choice2 == "8")) {
+        console.log("Matched telegram");
     } else {
-        console.log("Stupid");
+            selectedCard.disabled = "true";
+        ico.classList.remove("false");
     }
+
 
 
 }
